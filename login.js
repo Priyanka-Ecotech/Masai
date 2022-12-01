e8db0b611fc31a7613a8d433f00019eac00e6e03
Priyanka <you@example.com>
2022-12-01 14:45:54 +0530

    login_reg

---
+++ login.js
@@ -0,0 +1,15 @@
+// check whether a number is prime or not.
+
+let num=17;
+let count=0;
+for(let i=1; i<num ;i++){
+   if(num%1==0){
+     count++;
+}
+}
+if(count==1){
+   console.log("Yes")
+}
+else{
+   console.log("No")
+} 
